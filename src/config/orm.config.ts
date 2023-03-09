bc8819c6045e9ed27bce8f80d8d1c059110a536b
jinhwan7 <rkflql851@gmail.com>
2023-03-09 21:00:42 +0900

    [BE]poolsize 8

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -43,7 +43,7 @@ export default class TypeOrmConfig {
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: false,
-      // poolSize: 4,
+      poolSize: 8,
       // extra: {
       // max: 10,
       //   connectionLimit: 1,

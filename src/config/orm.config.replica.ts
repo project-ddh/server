e4bf8a7531284bec28bf75911b7d4d603aec907b
jinhwan7 <rkflql851@gmail.com>
2023-03-11 20:07:10 +0900

    테스트 코드 및 디비 호스트 route53으로 변경

--- src/config/orm.config.replica.ts
+++ src/config/orm.config.replica.ts
@@ -6,7 +6,7 @@ export default class TypeOrmReplicaConfig {
     return {
       keepConnectionAlive: true,
       type: 'postgres',
-      host: '118.67.134.26',
+      host: 'read.prachang.shop',
       port: configService.get('RDS_PORT'),
       username: configService.get('RDS_USERNAME'),
       password: configService.get('RDS_PASSWORD'),
